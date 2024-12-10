8328be1513c5a8b22129d8ff64e7275aa7587ee2
namakani01 <namakaninadar@gmail.com>
2024-12-10 15:02:28 +0530

    final change

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-
-import React from 'react'
-import { StatusBar,} from 'react-native'
-import store from './src/Redux/store/store'
-import { Provider } from 'react-redux'
-import Screen1 from './src/screens/screen1'
-
-
-
+import React from 'react';
+import {StatusBar} from 'react-native';
+import store from './src/Redux/store/store';
+import {Provider} from 'react-redux';
+import Screen1 from './src/screens/screen1';
 
 const App = () => {
   return (
-  <Provider store={store}>
-  <Screen1></Screen1>
-  <StatusBar backgroundColor={'#7860f0'}></StatusBar>
-  </Provider>
-  )
-}
+    <Provider store={store}>
+      <Screen1></Screen1>
+      <StatusBar backgroundColor={'#7860f0'}></StatusBar>
+    </Provider>
+  );
+};
 
-export default App
-
+export default App;
--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,7 +1,16 @@
-import { legacy_createStore } from "redux";
-import operationReducer from "../reducer/reducer";
+import {legacy_createStore, applyMiddleware} from 'redux';
+import operationReducer from '../reducer/reducer';
 
+function logger(store) {
+  return function (next) {
+    return function (action) {
+      console.log('store', store);
+      console.log('action', action);
+      next(action);
+    };
+  };
+}
 
-const store = legacy_createStore(operationReducer)
+const store = legacy_createStore(operationReducer, applyMiddleware(logger));
 
-export default store
+export default store;
